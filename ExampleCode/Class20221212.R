252954af21ca56a015ab23e17871930667a44ad2
tetokawata <keisukekawata7@gmail.com>
2022-12-12 19:05:52 +0900

    Add Class0221212

---
+++ ExampleCode/Class20221212.R
@@ -0,0 +1,38 @@
+set.seed(1)
+
+library(tidyverse)
+library(mlr3verse)
+library(DoubleML)
+
+Data <- read_csv("Example.csv")
+
+Data <- as.data.table(Data)
+
+Task <- double_ml_data_from_data_frame(
+  Data,
+  x_cols = c("TradeQ", "Size", "BuildYear", "Distance"),
+  d_cols = c("Reform"),
+  y_col = c("Price")
+)
+
+RegLearner <- lrn("regr.ranger")
+
+ProbLearner <- lrn("classif.ranger")
+# AIPW estimation
+Est <- DoubleMLIRM$new(Task,
+                RegLearner$clone(),
+                ProbLearner$clone())
+
+Est$fit()
+
+Est$summary()
+# Partialling-out estimation
+EstPRL <- DoubleMLPLR$new(Task,
+                       RegLearner$clone(),
+                       ProbLearner$clone())
+
+EstPRL$fit()
+
+EstPRL$summary()
+
+
